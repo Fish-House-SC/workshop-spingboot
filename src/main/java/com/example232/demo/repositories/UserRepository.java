45c4b35fe54f13846d1e987e76cf6756a80a7327
Fish-House-SC <estudiofishhouse@gmail.com>
2025-04-23 02:25:16 -0300

    Service Layer, component registration

--- src/main/java/com/example232/demo/repositories/UserRepository.java
+++ src/main/java/com/example232/demo/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example232.demo.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/example232/demo/services/UserService.java
@@ -0,0 +1,26 @@
+package com.example232.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example232.demo.entities.User;
+import com.example232.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User>FindAll(){
+		return repository.findAll();
+	}
+
+	public User FindById(Long id) {
+	 	Optional<User> obj = repository.findById(id);
+	 	return obj.get();
+	}
+}
