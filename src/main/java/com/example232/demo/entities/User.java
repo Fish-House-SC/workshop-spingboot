a90fd5350c6d9abff605f5534ab374772c67a201
Fish-House-SC <estudiofishhouse@gmail.com>
2025-05-01 00:55:00 -0300

    Order, Instant, ISO8601

--- src/main/java/com/example232/demo/entities/User.java
+++ src/main/java/com/example232/demo/entities/User.java
@@ -1,11 +1,14 @@
 package com.example232.demo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,12 @@ public class User implements Serializable {
 	    private String email;
 	    private String phone;
 	    private String password;
+	    
+	    
+	    @OneToMany(mappedBy = "client")
+	    private List<Order> orders = new ArrayList<>() ;
+	    
+	    
 	    
 		public User() {
 			super();
@@ -74,6 +83,10 @@ public class User implements Serializable {
 		public void setPassword(String password) {
 			this.password = password;
 		}
+		
+		public List<Order> getOrders() {
+			return orders;
+		}
 
 		@Override
 		public int hashCode() {
@@ -99,6 +112,8 @@ public class User implements Serializable {
 				return false;
 			return true;
 		}
+
+		
 		
 		
 	    
